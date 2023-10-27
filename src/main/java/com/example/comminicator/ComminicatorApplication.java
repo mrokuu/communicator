4a364ed96dce5df2ea14f2b2a7b4d5102d312bca
mrokuu <mroczkowskiadammm@gmail.com>
2023-10-27 14:58:10 +0200

    first commit

---
+++ src/main/java/com/example/comminicator/ComminicatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.comminicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComminicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComminicatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comminicator/ComminicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comminicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComminicatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
